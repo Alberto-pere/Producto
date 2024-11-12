d38b9cf345f0fff625436ecadebf36feeb4ea21a
Alberto-pere <utp0153616@alumno.utpuebla.edu.mx>
2024-11-11 20:06:57 -0600

    Create database.dart

---
+++ database.dart
@@ -0,0 +1,51 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  static Database? _database;
+
+  DatabaseHelper._privateConstructor();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'users.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+      )
+    ''');
+  }
+
+  Future<int> addUser(String username, String password) async {
+    Database db = await instance.database;
+    return await db
+        .insert('users', {'username': username, 'password': password});
+  }
+
+  Future<Map<String, dynamic>?> getUser(
+      String username, String password) async {
+    Database db = await instance.database;
+    List<Map<String, dynamic>> result = await db.query(
+      'users',
+      where: 'username = ? AND password = ?',
+      whereArgs: [username, password],
+    );
+    return result.isNotEmpty ? result.first : null;
+  }
+}
